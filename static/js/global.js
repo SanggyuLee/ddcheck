f5be68fc5c8bf3e52edbec3426aaefc089495dbb
이상규 <lp31004@gmail.com>
2016-11-01 19:35:50 +0900

    File names are modified
    * Script for motion is added.
    * TODO: 1. We should make UI for main page.
    	2. We should decide which DB will be used.

--- static/js/global.js
+++ static/js/global.js
@@ -1,6 +1,26 @@
+function appear(element) {
+	let opacity = 0;
+	function slowMotion() {
+		opacity += 0.02;
+		element.style.opacity = opacity;
+
+		if(opacity >= 1)
+			clearInterval(timer);
+	}
+
+	let timer = setInterval(slowMotion, 40);
+}
+
 document.body.addEventListener('click', function(event) {
 	if(event.target.id === "login-button") {
 		let password = document.getElementById("password").value;
-		alert(password);
+
+		/* TODO: Need to make password checking phase */
+		/*
+			if(!isPasswordValid())
+				return;
+		*/
+
+		window.location.assign("../main.html");
 	}
 });
