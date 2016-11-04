9d1babc5a2ab9dec78e15f6f5e8b90976f345398
SanggyuLee <lp31004@gmail.com>
2016-11-04 14:05:30 +0900

    Animation for password invalid is added

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
+const port = 80;
 
 app.use(express.static('static'));
 
-app.listen(3000, function () {
-	  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+	  console.log('Example app listening on port: ' + port);
 });
--- static/js/global.js
+++ static/js/global.js
@@ -1,4 +1,4 @@
-function appear(element) {
+let appear = (element) => {
 	let opacity = 0;
 	function slowMotion() {
 		opacity += 0.02;
@@ -9,17 +9,34 @@ function appear(element) {
 	}
 
 	let timer = setInterval(slowMotion, 40);
-}
+};
+
+let isPasswordValid = () => {
+	return false;
+};
 
 document.body.addEventListener('click', function(event) {
 	if(event.target.id === "login-button") {
 		let password = document.getElementById("password").value;
 
-		/* TODO: Need to make password checking phase */
-		/*
-			if(!isPasswordValid())
-				return;
-		*/
+		if(!isPasswordValid()) {
+			let loginForm = document.querySelector(".login");
+			let location = 18, num = 1;
+
+			let shake = () => {
+				location -= 0.3;
+				loginForm.style.marginLeft = location * num + "px";
+
+				num *= -1;
+
+				if(location <= 0)
+					clearInterval(timer);
+			};
+
+			let timer = setInterval(shake, 10);
+
+			return;
+		}
 
 		window.location.assign("../main.html");
 	}
